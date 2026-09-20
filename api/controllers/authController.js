896acf5e432e7aac44b90f9e72ae2d4b0808a4af
Marianne Nuorala <nuoralamarianne@gmail.com>
2026-09-20 22:47:07 +0300

    lisätty rekisteröinnin route index.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -18,7 +18,7 @@ export const register = async (req, res) => {
         }
 
         // Check that the password is at least 8 characters long and uses both numbers and letters
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
         if (!passwordRegex.test(password)) {
             return res.status(400).json({ message: "Password must be at least 8 characters long and it must contain numbers and letters"})
         }
